5de9b84340dc66b658844cf175045bbb54e6a673
Szymon Madeja <szmadeja@c2r4s1.42warsaw.pl>
2024-12-04 16:24:38 +0100

    a

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: szmadeja <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 18:29:16 by szmadeja          #+#    #+#             */
+/*   Updated: 2024/10/20 19:06:59 by szmadeja         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	while (power > 0)
+		return (nb * ft_recursive_power(nb, power - 1));
+	return (1);
+}
